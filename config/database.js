0b6f11efaa3e24500c3d54f15743bdc39c85f405
Luke Baal <luke.baal@uoit.net>
2019-01-29 10:06:35 -0500

    Updated production db host

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 module.exports = {
   production: new Sequelize('ospv', 'postgres', 'example', {
-    host: 'localhost',
+    host:
+      'postgres://nvcthjgztcfoxn:017267e4e7246681d14062846bd33804ac725f580238105a6867503eef03e69e@ec2-23-23-184-76.compute-1.amazonaws.com:5432/d4nvvaqq8dql77',
     dialect: 'postgres',
     operatorsAliases: false,
     pool: {
